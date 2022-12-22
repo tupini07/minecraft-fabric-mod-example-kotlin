15a02bf61937c55a1ba895117bac2e3779a57584
Andrea Tupini <tupini07@gmail.com>
2022-12-22 16:48:42 -0600

    clean up packet listener

--- src/main/kotlin/net/fabricmc/example/client/utils/packets/ExPacketListener.kt
+++ src/main/kotlin/net/fabricmc/example/client/utils/packets/ExPacketListener.kt
@@ -55,41 +55,34 @@ class ExPacketListener(name: String) : ChannelInboundHandlerAdapter() {
         // TODO: How do we know which `tick` number the current event happened in?
         //  ^ Consider that this code here runs on the network thread
 
-        if (msg is ByteBuf) {
-            val byteBuffer: ByteBuffer = msg.copyToByteBufferSafe()
-            val packet = serializer.writeValueAsString(byteBuffer)
-            bufferedWriter.write(packet)
+        val byteBuffer: ByteBuffer = if (msg is ByteBuf) {
+            msg.copyToByteBufferSafe()
         } else {
-            val encoded = encodePacket(ctx, msg as Packet<*>)
-
-            // NOTE: it's a bit weird but the `serialized` output is enclosed in double quotes, and these are
-            // necessary for the deserializer to work properly!
-            val serialized = serializer.writeValueAsString(encoded!!.copyToByteBufferSafe())
-
-            // NOTE: The below can be used to re-read the "serialized" packet
-//                // verify that deserialization works properly as well
-//                val pdec = ExPacketDecoder(PacketFlow.CLIENTBOUND)
-//                val justDecoded = pdec.exDecode(ctx, encoded.copy())
-//
-//                // try to re-decode
-//                val deserializedAndDecoded = serializer
-//                    .readValue(serialized, ByteBuffer::class.java)
-//                    .let {
-//                        pdec.exDecode(ctx, it.into<ByteBuf>())
-//                    }
-
-            // there's no need to include the class name in the output, since it's already in the packet (the packet id)
-            bufferedWriter.write(serialized)
+            encodePacket(ctx, msg as Packet<*>)!!.copyToByteBufferSafe()
         }
 
+        // NOTE: it's a bit weird but the `serialized` output is enclosed in double quotes, and these are
+        // necessary for the deserializer to work properly!
+        val serializedBuffer = serializer.writeValueAsString(byteBuffer)
+
+        bufferedWriter.write(serializedBuffer)
+
+        // to decode packets
+        // val deserializedPacket = serializer
+        //     .readValue(serializedBuffer, ByteBuffer::class.java)
+        //     .let {
+        //         val byteBuf = it.into<ByteBuf>()
+        //         decodePacket(ctx, byteBuf)
+        //     }
+
+        // TODO: this might be unnecessary and inneficient to read. Better to have a stream parser that ueses
+        // double quotes (") to know when a packet starts and ends
         bufferedWriter.write("\n")
 
         this.numWrites += 1
-        if (this.numWrites == 1000) {
-            // TODO: for performance, flush every 100 writes
+        if (this.numWrites % 1000 == 0) {
             bufferedWriter.flush()
-            LOGGER.info("Flushed $numWrites writes")
-            this.numWrites = 0
+            LOGGER.info("Flushed packets. Total= $numWrites")
         }
 
         ctx.fireChannelRead(msg)
